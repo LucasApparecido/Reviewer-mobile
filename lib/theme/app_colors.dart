8832e6046739fe104562b8b5938b48f1da560302
Jonathan Araújo Rodrigues dos Santos <100201123+JoJonata@users.noreply.github.com>
2025-04-28 14:22:14 -0300

    Primeiro commit - Definindo a paleta de cores.

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color background = Color(0xFFFAF3E0);
+  static const Color primary = Color(0xFFFFC107);
+  static const Color primaryVariant = Color(0xFFFFD700);
+  static const Color accent = Color(0xFFFF4C4C);
+  static const Color darkText = Color(0xFF222222);
+  static const Color mediumText = Color(0xFF555555);
+  static const Color lightGray = Color(0xFFDDDDDD);
+}
