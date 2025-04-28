c2e22a6b0e8a26655d932ed5ab59e08ff691e2f0
Jonathan Araújo Rodrigues dos Santos <100201123+JoJonata@users.noreply.github.com>
2025-04-28 14:35:12 -0300

    Commit complementar

---
+++ android/app/src/main/kotlin/br/ueg/reviewer/reviewer_mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package br.ueg.reviewer.reviewer_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:routefly/routefly.dart';
+import 'main.route.dart'; // GERADO automaticamente depois
+
+part 'main.g.dart'; // Define o arquivo gerado
+
+void main() {
+  runApp(const ReviewSocialApp());
+}
+
+@Main()
+class ReviewSocialApp extends StatelessWidget {
+  const ReviewSocialApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerConfig: Routefly.routerConfig(
+        routes: routes, // Rota gerada automaticamente
+        initialPath: routePaths.appHome
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
